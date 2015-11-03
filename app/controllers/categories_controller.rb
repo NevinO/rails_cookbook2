f050a23349e47582040cc453bcd4c4245cbf8ee1
NSO <officer4hire@gmail.com>
2015-11-03 08:56:49 +0000

    validations etc

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,44 +1,46 @@
 class CategoriesController < ApplicationController
 
-  def category_params
-    params.require(:category).permit(:name)
-  end
-
-  def create
-    Category.create(category_params)
-    redirect_to(categories_path)
-  end
-
-  def destroy
-    category = Category.find(params[:id])
-    category.destroy
-    redirect_to(categories_path)
-  end
-
-  def edit
-    @category = Category.find(params[:id])
-  end
-
   def index
     @categories = Category.all.order(:name)
+  end
+
+  def show
+    @category = find_category
   end
 
   def new
     @category = Category.new
   end
 
-  def show
-    @category = Category.find(params[:id])
+  def create
+    category = Category.create(category_params)
+    redirect_to(category_path(category.id))
+  end
+
+  def destroy
+    category = find_category
+    category.destroy
+    redirect_to(categories_path)
+  end
+
+  def edit
+    @category = find_category
   end
 
   def update
-    category = Category.find(params[:id])
+    category = find_category
     category.update(category_params)
-    redirect_to(categories_path)
+    redirect_to(category_path(category.id))
   end
 
   private
 
-  
+  def find_category
+    Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030153802) do
+ActiveRecord::Schema.define(version: 20151031170238) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
